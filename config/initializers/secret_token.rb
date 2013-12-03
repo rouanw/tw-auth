6d8daf5dc5b1f642e9186383365de89bb8f08cfb
Rouan Wilsenach <rouanw@gmail.com>
2013-12-03 15:10:18 +0200

    rails with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twauth::Application.config.secret_token = '8c52dc1f5a0e9266f210842f61ab28d79f8889e18bf373e72ffb183391eee5bf6949eac35342cd08a86d4dfff3eeb26841759662180e81f059dd6121aab96ea5'
