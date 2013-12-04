a6c88123921d0428af9d77ebd50f8064bde73d1e
Rouan Wilsenach <rouanw@gmail.com>
2013-12-04 11:48:08 +0200

    add confirmable devise to user  model

--- Gemfile
+++ Gemfile
@@ -8,6 +8,10 @@ gem 'rails', '3.2.12'
 gem 'sqlite3'
 gem 'devise'
 
+group :development do
+  gem 'mailcatcher'
+end
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Twauth::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'smtp://127.0.0.1:1025' }
 end
